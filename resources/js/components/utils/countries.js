40a13b8dc9deb281ec17e0e9d47871c5622f3474
Decencies <66835910+Decencies@users.noreply.github.com>
2024-03-12 12:05:58 +0000

    Initial Commit

---
+++ resources/js/components/utils/countries.js
@@ -0,0 +1,706 @@
+var countries = [{
+  option: 'Afghanistan',
+  value: 'AF'
+}, {
+  option: 'Ã…land Islands',
+  value: 'AX'
+}, {
+  option: 'Albania',
+  value: 'AL'
+}, {
+  option: 'Algeria',
+  value: 'DZ'
+}, {
+  option: 'Andorra',
+  value: 'AD'
+}, {
+  option: 'Angola',
+  value: 'AO'
+}, {
+  option: 'Anguilla',
+  value: 'AI'
+}, {
+  option: 'Antarctica',
+  value: 'AQ'
+}, {
+  option: 'Antigua &amp; Barbuda',
+  value: 'AG'
+}, {
+  option: 'Argentina',
+  value: 'AR'
+}, {
+  option: 'Armenia',
+  value: 'AM'
+}, {
+  option: 'Aruba',
+  value: 'AW'
+}, {
+  option: 'Ascension Island',
+  value: 'AC'
+}, {
+  option: 'Australia',
+  value: 'AU'
+}, {
+  option: 'Austria',
+  value: 'AT'
+}, {
+  option: 'Azerbaijan',
+  value: 'AZ'
+}, {
+  option: 'Bahamas',
+  value: 'BS'
+}, {
+  option: 'Bahrain',
+  value: 'BH'
+}, {
+  option: 'Bangladesh',
+  value: 'BD'
+}, {
+  option: 'Barbados',
+  value: 'BB'
+}, {
+  option: 'Belarus',
+  value: 'BY'
+}, {
+  option: 'Belgium',
+  value: 'BE'
+}, {
+  option: 'Belize',
+  value: 'BZ'
+}, {
+  option: 'Benin',
+  value: 'BJ'
+}, {
+  option: 'Bermuda',
+  value: 'BM'
+}, {
+  option: 'Bhutan',
+  value: 'BT'
+}, {
+  option: 'Bolivia',
+  value: 'BO'
+}, {
+  option: ' Bosnia &amp; Herzegovina',
+  value: 'BA'
+}, {
+  option: 'Botswana',
+  value: 'BW'
+}, {
+  option: 'Bouvet Island',
+  value: 'BV'
+}, {
+  option: 'Brazil',
+  value: 'BR'
+}, {
+  option: 'British Indian Ocean Territory',
+  value: 'IO'
+}, {
+  option: 'British Virgin Islands',
+  value: 'VG'
+}, {
+  option: 'Brunei',
+  value: 'BN'
+}, {
+  option: 'Bulgaria',
+  value: 'BG'
+}, {
+  option: 'Burkina Faso',
+  value: 'BF'
+}, {
+  option: 'Burundi',
+  value: 'BI'
+}, {
+  option: 'Cambodia',
+  value: 'KH'
+}, {
+  option: 'Cameroon',
+  value: 'CM'
+}, {
+  option: 'Canada',
+  value: 'CA'
+}, {
+  option: 'Cape Verde',
+  value: 'CV'
+}, {
+  option: 'Caribbean Netherlands',
+  value: 'BQ'
+}, {
+  option: 'Cayman Islands',
+  value: 'KY'
+}, {
+  option: 'Central African Republic',
+  value: 'CF'
+}, {
+  option: 'Chad',
+  value: 'TD'
+}, {
+  option: 'Chile',
+  value: 'CL'
+}, {
+  option: 'China',
+  value: 'CN'
+}, {
+  option: 'Colombia',
+  value: 'CO'
+}, {
+  option: 'Comoros',
+  value: 'KM'
+}, {
+  option: ' Congo - Brazzaville ',
+  value: 'CG'
+}, {
+  option: 'Congo - Kinshasa',
+  value: 'CD'
+}, {
+  option: 'Cook Islands',
+  value: 'CK'
+}, {
+  option: 'Costa Rica',
+  value: 'CR'
+}, {
+  option: 'Croatia ',
+  value: 'HR'
+}, {
+  option: 'CuraÃ§ao',
+  value: 'CW'
+}, {
+  option: 'Cyprus ',
+  value: 'CY'
+}, {
+  option: 'Czechia ',
+  value: 'CZ'
+}, {
+  option: 'Denmark ',
+  value: 'DK'
+}, {
+  option: 'Djibouti ',
+  value: 'DJ'
+}, {
+  option: 'Dominica ',
+  value: 'DM'
+}, {
+  option: 'Dominican Republic',
+  value: 'DO'
+}, {
+  option: 'Ecuador ',
+  value: 'EC'
+}, {
+  option: 'Egypt ',
+  value: 'EG'
+}, {
+  option: 'El Salvador ',
+  value: 'SV'
+}, {
+  option: 'Equatorial Guinea',
+  value: 'GQ'
+}, {
+  option: 'Eritrea ',
+  value: 'ER'
+}, {
+  option: 'Estonia ',
+  value: 'EE'
+}, {
+  option: 'Eswatini ',
+  value: 'SZ'
+}, {
+  option: 'Ethiopia ',
+  value: 'ET'
+}, {
+  option: 'Falkland Islands',
+  value: 'FK'
+}, {
+  option: 'Faroe Islands',
+  value: 'FO'
+}, {
+  option: 'Fiji',
+  value: 'FJ'
+}, {
+  option: 'Finland',
+  value: 'FI'
+}, {
+  option: 'France',
+  value: 'FR'
+}, {
+  option: 'French Guiana',
+  value: 'GF'
+}, {
+  option: 'French Polynesia',
+  value: 'PF'
+}, {
+  option: 'French Southern Territories',
+  value: 'TF'
+}, {
+  option: 'Gabon',
+  value: 'GA'
+}, {
+  option: 'Gambia',
+  value: 'GM'
+}, {
+  option: 'Georgia',
+  value: 'GE'
+}, {
+  option: 'Germany',
+  value: 'DE'
+}, {
+  option: 'Ghana',
+  value: 'GH'
+}, {
+  option: 'Gibraltar',
+  value: 'GI'
+}, {
+  option: 'Greece',
+  value: 'GR'
+}, {
+  option: 'Greenland',
+  value: 'GL'
+}, {
+  option: 'Grenada',
+  value: 'GD'
+}, {
+  option: 'Guadeloupe',
+  value: 'GP'
+}, {
+  option: 'Guam',
+  value: 'GU'
+}, {
+  option: 'Guatemala',
+  value: 'GT'
+}, {
+  option: 'Guernsey',
+  value: 'GG'
+}, {
+  option: 'Guinea',
+  value: 'GN'
+}, {
+  option: 'Guinea - Bissau',
+  value: 'GW'
+}, {
+  option: 'Guyana',
+  value: 'GY'
+}, {
+  option: 'Haiti',
+  value: 'HT'
+}, {
+  option: 'Honduras',
+  value: 'HN'
+}, {
+  option: 'Hong Kong SAR China',
+  value: 'HK'
+}, {
+  option: 'Hungary',
+  value: 'HU'
+}, {
+  option: 'Iceland',
+  value: 'IS'
+}, {
+  option: 'India',
+  value: 'IN'
+}, {
+  option: 'Indonesia',
+  value: 'ID'
+}, {
+  option: 'Iran',
+  value: 'IR'
+}, {
+  option: 'Ireland',
+  value: 'IE'
+}, {
+  option: 'Isle of Man',
+  value: 'IM'
+}, {
+  option: 'Israel',
+  value: 'IL'
+}, {
+  option: 'Italy',
+  value: 'IT'
+}, {
+  option: 'Jamaica',
+  value: 'JM'
+}, {
+  option: 'Japan',
+  value: 'JP'
+}, {
+  option: 'Jersey',
+  value: 'JE'
+}, {
+  option: 'Jordan',
+  value: 'JO'
+}, {
+  option: 'Kazakhstan',
+  value: 'KZ'
+}, {
+  option: 'Kenya',
+  value: 'KE'
+}, {
+  option: 'Kiribati',
+  value: 'KI'
+}, {
+  option: 'Kosovo',
+  value: 'XK'
+}, {
+  option: 'Kuwait',
+  value: 'KW'
+}, {
+  option: 'Kyrgyzstan',
+  value: 'KG'
+}, {
+  option: 'Laos',
+  value: 'LA'
+}, {
+  option: 'Latvia',
+  value: 'LV'
+}, {
+  option: 'Lebanon',
+  value: 'LB'
+}, {
+  option: 'Lesotho',
+  value: 'LS'
+}, {
+  option: 'Liberia',
+  value: 'LR'
+}, {
+  option: 'Libya',
+  value: 'LY'
+}, {
+  option: 'Liechtenstein',
+  value: 'LI'
+}, {
+  option: 'Lithuania',
+  value: 'LT'
+}, {
+  option: 'Luxembourg',
+  value: 'LU'
+}, {
+  option: 'Macao SAR China',
+  value: 'MO'
+}, {
+  option: 'Madagascar',
+  value: 'MG'
+}, {
+  option: 'Malawi',
+  value: 'MW'
+}, {
+  option: 'Malaysia',
+  value: 'MY'
+}, {
+  option: 'Maldives',
+  value: 'MV'
+}, {
+  option: 'Mali',
+  value: 'ML'
+}, {
+  option: 'Malta',
+  value: 'MT'
+}, {
+  option: 'Martinique',
+  value: 'MQ'
+}, {
+  option: 'Mauritania',
+  value: 'MR'
+}, {
+  option: 'Mauritius',
+  value: 'MU'
+}, {
+  option: 'Mayotte',
+  value: 'YT'
+}, {
+  option: 'Mexico',
+  value: 'MX'
+}, {
+  option: 'Moldova',
+  value: 'MD'
+}, {
+  option: 'Monaco',
+  value: 'MC'
+}, {
+  option: 'Mongolia',
+  value: 'MN'
+}, {
+  option: 'Montenegro',
+  value: 'ME'
+}, {
+  option: 'Montserrat',
+  value: 'MS'
+}, {
+  option: 'Morocco',
+  value: 'MA'
+}, {
+  option: 'Mozambique',
+  value: 'MZ'
+}, {
+  option: 'Myanmar(Burma)',
+  value: 'MM'
+}, {
+  option: 'Namibia',
+  value: 'NA'
+}, {
+  option: 'Nauru',
+  value: 'NR'
+}, {
+  option: 'Nepal',
+  value: 'NP'
+}, {
+  option: 'Netherlands',
+  value: 'NL'
+}, {
+  option: 'New Caledonia',
+  value: 'NC'
+}, {
+  option: 'New Zealand',
+  value: 'NZ'
+}, {
+  option: 'Nicaragua',
+  value: 'NI'
+}, {
+  option: 'Niger',
+  value: 'NE'
+}, {
+  option: 'Nigeria',
+  value: 'NG'
+}, {
+  option: 'Niue',
+  value: 'NU'
+}, {
+  option: 'North Macedonia',
+  value: 'MK'
+}, {
+  option: 'Norway',
+  value: 'NO'
+}, {
+  option: 'Oman',
+  value: 'OM'
+}, {
+  option: 'Pakistan',
+  value: 'PK'
+}, {
+  option: 'Palestinian Territories',
+  value: 'PS'
+}, {
+  option: 'Panama',
+  value: 'PA'
+}, {
+  option: 'Papua New Guinea',
+  value: 'PG'
+}, {
+  option: 'Paraguay',
+  value: 'PY'
+}, {
+  option: 'Peru',
+  value: 'PE'
+}, {
+  option: 'Philippines',
+  value: 'PH'
+}, {
+  option: 'Pitcairn Islands',
+  value: 'PN'
+}, {
+  option: 'Poland',
+  value: 'PL'
+}, {
+  option: 'Portugal',
+  value: 'PT'
+}, {
+  option: 'Puerto Rico',
+  value: 'PR'
+}, {
+  option: 'Qatar',
+  value: 'QA'
+}, {
+  option: 'RÃ©union',
+  value: 'RE'
+}, {
+  option: 'Romania',
+  value: 'RO'
+}, {
+  option: 'Russia',
+  value: 'RU'
+}, {
+  option: 'Rwanda',
+  value: 'RW'
+}, {
+  option: 'Samoa',
+  value: 'WS'
+}, {
+  option: 'San Marino',
+  value: 'SM'
+}, {
+  option: 'SÃ£o TomÃ© &amp; PrÃ­ncipe',
+  value: 'ST'
+}, {
+  option: 'Saudi Arabia',
+  value: 'SA'
+}, {
+  option: 'Senegal',
+  value: 'SN'
+}, {
+  option: 'Serbia',
+  value: 'RS'
+}, {
+  option: 'Seychelles',
+  value: 'SC'
+}, {
+  option: 'Sierra Leone',
+  value: 'SL'
+}, {
+  option: 'Singapore',
+  value: 'SG'
+}, {
+  option: 'Sint Maarten',
+  value: 'SX'
+}, {
+  option: 'Slovakia',
+  value: 'SK'
+}, {
+  option: 'Slovenia',
+  value: 'SI'
+}, {
+  option: 'Solomon Islands',
+  value: 'SB'
+}, {
+  option: 'Somalia',
+  value: 'SO'
+}, {
+  option: 'South Africa',
+  value: 'ZA'
+}, {
+  option: 'South Georgia &amp; South Sandwich Islands',
+  value: 'GS'
+}, {
+  option: 'South Korea',
+  value: 'KR'
+}, {
+  option: 'South Sudan',
+  value: 'SS'
+}, {
+  option: 'Spain',
+  value: 'ES'
+}, {
+  option: 'Sri Lanka',
+  value: 'LK'
+}, {
+  option: 'St.BarthÃ©lemy',
+  value: 'BL'
+}, {
+  option: 'St.Helena',
+  value: 'SH'
+}, {
+  option: 'St.Kitts &amp; Nevis',
+  value: 'KN'
+}, {
+  option: 'St.Lucia',
+  value: 'LC'
+}, {
+  option: 'St.Martin',
+  value: 'MF'
+}, {
+  option: 'St.Pierre &amp; Miquelon',
+  value: 'PM'
+}, {
+  option: 'St.Vincent &amp; Grenadines',
+  value: 'VC'
+}, {
+  option: 'Suriname',
+  value: 'SR'
+}, {
+  option: 'Svalbard &amp; Jan Mayen',
+  value: 'SJ'
+}, {
+  option: 'Sweden',
+  value: 'SE'
+}, {
+  option: 'Switzerland',
+  value: 'CH'
+}, {
+  option: 'Taiwan',
+  value: 'TW'
+}, {
+  option: 'Tajikistan',
+  value: 'TJ'
+}, {
+  option: 'Tanzania',
+  value: 'TZ'
+}, {
+  option: 'Thailand',
+  value: 'TH'
+}, {
+  option: 'Timor - Leste',
+  value: 'TL'
+}, {
+  option: 'Togo',
+  value: 'TG'
+}, {
+  option: 'Tokelau',
+  value: 'TK'
+}, {
+  option: 'Tonga',
+  value: 'TO'
+}, {
+  option: 'Trinidad &amp; Tobago',
+  value: 'TT'
+}, {
+  option: 'Tristan da Cunha',
+  value: 'TK'
+}, {
+  option: 'Tunisia',
+  value: 'TN'
+}, {
+  option: 'Turkey',
+  value: 'TR'
+}, {
+  option: 'Turkmenistan',
+  value: 'TM'
+}, {
+  option: 'Turks &amp; Caicos Islands',
+  value: 'TC'
+}, {
+  option: 'Tuvalu',
+  value: 'TV'
+}, {
+  option: 'Uganda',
+  value: 'UG'
+}, {
+  option: 'Ukraine',
+  value: 'UA'
+}, {
+  option: 'United Arab Emirates',
+  value: 'AE'
+}, {
+  option: 'United Kingdom',
+  value: 'GB'
+}, {
+  option: 'United States',
+  value: 'US'
+}, {
+  option: 'Uruguay',
+  value: 'UY'
+}, {
+  option: 'Uzbekistan',
+  value: 'UZ'
+}, {
+  option: 'Vanuatu',
+  value: 'VU'
+}, {
+  option: 'Vatican City',
+  value: 'VA'
+}, {
+  option: 'Venezuela',
+  value: 'VE'
+}, {
+  option: 'Vietnam',
+  value: 'VN'
+}, {
+  option: ' Wallis &amp; Futuna ',
+  value: 'WF'
+}, {
+  option: 'Western Sahara',
+  value: 'EH'
+}, {
+  option: 'Yemen',
+  value: 'YE'
+}, {
+  option: 'Zambia',
+  value: 'ZM'
+}, {
+  option: 'Zimbabwe',
+  value: 'ZW'
+}];
